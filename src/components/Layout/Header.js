9258d3bf70b182327b15d7250e7467205cd339ea
Abd Hannan <user@192.168.1.4>
2023-01-28 01:23:28 +0700

    Firs setup

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div></div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
